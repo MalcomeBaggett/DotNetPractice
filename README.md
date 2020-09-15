1150c7032e26774dd2bbf495a6eb89d3a15e72bc
Malcome Baggett <malcomebaggett@gmail.com>
2020-09-15 16:07:38 -0700

    Init Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# C# and .Net Refresher
+
+## Welcome!
+
+The purpose of this repository is to hold small to medium sized projects and it will also be a place where I can practice syntax and other c# related topics without poluting my github profile.
---
+++ c#basics/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace c_basics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Do and While Loops
+            // Do loops run atleast once
+            // int tries = 0;
+            // int problemsSolved = 0;
+            // do
+            // {
+            //     Console.WriteLine($"on try {tries}");
+            //     tries += 1;
+            // } while (tries < 10);
+
+            // While loops do not run unless condition is met
+            // while (problemsSolved < 11)
+            // {
+            //     Console.WriteLine($"You currently have solved {problemsSolved} problems");
+            //     problemsSolved += 1;
+            // }
+            //     string result = "";
+            //     string firstName;
+            //     do
+            //     {
+            //         Console.WriteLine("What is your first name user?");
+            //         firstName = Console.ReadLine();
+            //         Console.WriteLine($"Hello user {firstName}, do you wish to continue? Enter yes or no.");
+            //         result = Console.ReadLine();
+            //     } while (result.ToLower() == "yes");
+            //     Console.ReadLine();
+        }
+    }
+}
