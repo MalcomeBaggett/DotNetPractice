5ef6c319a7fbdb7c3a53f0ff60f2c446b467050d
Malcome Baggett <malcomebaggett@gmail.com>
2020-09-22 12:24:57 -0700

    Completed Minimun Req for MiniProjOne

--- MiniProjOne/Program.cs
+++ MiniProjOne/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace MiniProjOne
 {
@@ -6,7 +7,24 @@ namespace MiniProjOne
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Dictionary<int, string> EmployeeIdDict = new Dictionary<int, string>();
+            string employeeID;
+            EmployeeIdDict[1] = "Malcome Baggett";
+            EmployeeIdDict[2] = "Fiona Baggett";
+            Console.WriteLine("Welcome to GreenBox Farm's Employee Look Up");
+            Console.WriteLine("Please Enter an Employee ID to lookup");
+            employeeID = Console.ReadLine();
+            bool validatedID = int.TryParse(employeeID, out int validID);
+
+            if (validatedID == true)
+            {
+                if (EmployeeIdDict.ContainsKey(validID))
+                {
+                    Console.WriteLine(EmployeeIdDict[validID]);
+                }
+            }
+
+            Console.ReadLine();
         }
     }
 }
