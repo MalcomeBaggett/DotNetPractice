4dfd9089224b42bffd024890f1dbeee71fe16dfd
Malcome Baggett <malcomebaggett@gmail.com>
2020-09-23 14:28:29 -0700

    added miniproj1

---
+++ MiniProjectSectionOne/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MiniProjectSectionOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool endSession = false;
+            Console.WriteLine("Welcome to our self serve guest list!");
+            Console.WriteLine("Please follow the prompts below to ensure the best check in experience");
+            while (endSession == false)
+            {
+                string userName = GetName();
+                int partyCount = GetPartyCount();
+                AddParty(userName, partyCount);
+            }
+        }
+        public Dictionary<string, int> guestBook = new Dictionary<string, int>();
+        private static string GetName()
+        {
+            Console.WriteLine("Please enter your name.");
+            string name = Console.ReadLine();
+            return name;
+        }
+        private static int GetPartyCount()
+        {
+            Console.WriteLine("Please enter how many people are in your party");
+            string partyNumString = Console.ReadLine();
+            bool isValidInt = int.TryParse(partyNumString, out int partyNum);
+            if (isValidInt == true)
+            {
+                return partyNum;
+            }
+            return 1;
+        }
+        private static void AddParty(string userName, int partyCount)
+        {
+            guestBook[userName] = partyCount;
+            Console.WriteLine($"Your Party has been logged {userName}, please proceed.");
+        }
+    }
+}
---
+++ MiniProjectSectionOne/README.md
@@ -0,0 +1,7 @@
+# Mini Project Section One
+
+## Reqs
+
+Build a Console Guest Book. Ask user for their name and how many are in there party
+Keep track of how many people are in there party.
+At the end print out the guest list and total number of guests
