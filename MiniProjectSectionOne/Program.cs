d0a10b10c9ca0a15761f66cf00f2c315982f1536
Malcome Baggett <malcomebaggett@gmail.com>
2020-09-30 21:35:24 -0700

    Finished MiniProjOne

--- MiniProjectSectionOne/Program.cs
+++ MiniProjectSectionOne/Program.cs
@@ -5,17 +5,38 @@ namespace MiniProjectSectionOne
 {
     class Program
     {
+        struct party
+        {
+            public string guestName;
+            public int partySize;
+        }
         static void Main(string[] args)
         {
-            var sessionDict = Program.guestBook();
+            List<party> partySession = new List<party>();
             bool endSession = false;
             Console.WriteLine("Welcome to our self serve guest list!");
             Console.WriteLine("Please follow the prompts below to ensure the best check in experience");
             while (endSession == false)
             {
-                string userName = GetName();
-                int partyCount = GetPartyCount();
-                Program.AddParty(sessionDict, userName, partyCount);
+                party guest = new party();
+                guest.guestName = GetName();
+                guest.partySize = GetPartyCount();
+                partySession.Add(guest);
+
+                Console.WriteLine("Would you like to add another Guest? Yes or No?");
+                string answer = Console.ReadLine();
+
+                if (answer.ToLower() == "no")
+                {
+                    endSession = true;
+                }
+                int count = 0;
+                foreach (var item in partySession)
+                {
+                    count += item.partySize;
+                    Console.Write($"{item.guestName} ");
+                }
+                Console.WriteLine($"\nThe total number of guests is {count}");
             }
         }
         public Dictionary<string, int> guestBook = new Dictionary<string, int>();
@@ -35,12 +56,6 @@ namespace MiniProjectSectionOne
                 return partyNum;
             }
             return 1;
-        }
-        private static Dictionary<int, string> AddParty(dictionary<int, string> guestBook, string userName, int partyCount)
-        {
-            guestBook[userName] = partyCount;
-            Console.WriteLine($"Your Party has been logged {userName}, please proceed.");
-            return guestBook;
         }
     }
 }
